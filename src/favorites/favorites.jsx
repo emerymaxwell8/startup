73fc8e6710cfe6d3675efb73b7668fa8fb7d7bb4
Emery Maxwell <emerymaxwell8@gmail.com>
2026-02-11 13:26:36 -0700

    fix main in favorites.jsx and also head in index

--- src/favorites/favorites.jsx
+++ src/favorites/favorites.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import './favorites.css';
 
 export function Favorites() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
+    <main class="favorites-main">
+        <h1 class="title"> YOUR FAVORITES!</h1>
+        <div>
+            <table class="table table-primary table-striped-columns">
+                <thead>
+                    <tr>
+                        <th>NAME</th>
+                        <th>DINNER PLAN</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Nico</td>
+                        <td>making fish tacos!</td>
+                    </tr>
+                    <tr>
+                        <td>Addie</td>
+                        <td>I love a good rice bowl</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </main>
   );
 }
