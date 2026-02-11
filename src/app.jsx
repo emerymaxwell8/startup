bf7727c94e823e1267cbed7d3d81257277b09243
Emery Maxwell <emerymaxwell8@gmail.com>
2026-02-11 11:37:58 -0700

    add header and footer components into app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="app bg-dark text-light">App will display here</div>;
+  return (
+    <div className="app">
+      <header>
+            <nav className="navbar">
+                <h1 className="navbar-brand fs-2 text-white">TASTY MEALS</h1>
+                <menu className="navbar-nav">
+                    <li className="nav-item"><a className ="nav-link active" href="index.html">HOME</a></li>
+                    <li className="nav-item"><a className="nav-link" href="post.html">POST</a></li>
+                    <li className="nav-item"><a className="nav-link" href="favorites.html">FAVORITES</a></li>
+                    <li className="nav-item"><a className="nav-link" href="about.html">ABOUT</a></li>
+                </menu>
+            </nav>
+        </header>
+
+      <main>App components go here</main>
+
+      <footer className = 'text-white'>
+            <div>
+                <span>Emery Maxwell</span>
+                <a href="https://github.com/emerymaxwell8/startup.git">GitHub</a>
+            </div>
+        </footer>
+    </div>
+  );
 }
