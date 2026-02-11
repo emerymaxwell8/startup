d6854b6f038714bb1fa4263997246d3c96c2fd0f
Emery Maxwell <emerymaxwell8@gmail.com>
2026-02-11 11:43:19 -0700

    add imports and wrap with BrowserRouter

--- src/app.jsx
+++ src/app.jsx
@@ -1,30 +1,37 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { Scores } from './scores/scores';
+import { About } from './about/about';
 
 export default function App() {
   return (
-    <div className="app">
-      <header>
-            <nav className="navbar">
-                <h1 className="navbar-brand fs-2 text-white">TASTY MEALS</h1>
-                <menu className="navbar-nav">
-                    <li className="nav-item"><a className ="nav-link active" href="index.html">HOME</a></li>
-                    <li className="nav-item"><a className="nav-link" href="post.html">POST</a></li>
-                    <li className="nav-item"><a className="nav-link" href="favorites.html">FAVORITES</a></li>
-                    <li className="nav-item"><a className="nav-link" href="about.html">ABOUT</a></li>
-                </menu>
-            </nav>
-        </header>
+    <BrowserRouter>
+        <div className="app">
+        <header>
+                <nav className="navbar">
+                    <h1 className="navbar-brand fs-2 text-white">TASTY MEALS</h1>
+                    <menu className="navbar-nav">
+                        <li className="nav-item"><a className ="nav-link active" href="index.html">HOME</a></li>
+                        <li className="nav-item"><a className="nav-link" href="post.html">POST</a></li>
+                        <li className="nav-item"><a className="nav-link" href="favorites.html">FAVORITES</a></li>
+                        <li className="nav-item"><a className="nav-link" href="about.html">ABOUT</a></li>
+                    </menu>
+                </nav>
+            </header>
 
-      <main>App components go here</main>
+        <main>App components go here</main>
 
-      <footer className = 'text-white'>
-            <div>
-                <span>Emery Maxwell</span>
-                <a href="https://github.com/emerymaxwell8/startup.git">GitHub</a>
-            </div>
-        </footer>
-    </div>
+        <footer className = 'text-white'>
+                <div>
+                    <span>Emery Maxwell</span>
+                    <a href="https://github.com/emerymaxwell8/startup.git">GitHub</a>
+                </div>
+            </footer>
+        </div>
+    </BrowserRouter>
   );
 }
