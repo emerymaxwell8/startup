126f3093400a30b57724765e91274bb21e49ae74
Emery Maxwell <emerymaxwell8@gmail.com>
2026-02-11 13:32:06 -0700

    make login.jsx and fix number

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 
 export function Login() {
   return (
-    <main class="row-main">
-        <div><img class="picture" alt = "pizza" width = 300px src="pizza-holder.jpg" /></div>
+    <main className="row-main">
+        <div><img className="picture" alt = "pizza" src="pizza-holder.jpg" width = {300}px /></div>
         <div>
-            <h1 id='title-main' class="title">Welcome!</h1>
+            <h1 id='title-main' className="title">Welcome!</h1>
             <form method="get" action="post.html">
-                <div class="input-group mb-3">
-                    <span class="input-group-text">EMAIL</span>
-                    <input class="form-control" type="text" placeholder="name@email.com" />
+                <div className="input-group mb-3">
+                    <span className="input-group-text">EMAIL</span>
+                    <input className="form-control" type="text" placeholder="name@email.com" />
                 </div>
-                <div class="input-group mb-3">
-                    <span class="input-group-text">PASSWORD</span>
-                    <input class="form-control" type="password" placeholder="password" />
+                <div className="input-group mb-3">
+                    <span className="input-group-text">PASSWORD</span>
+                    <input className="form-control" type="password" placeholder="password" />
                 </div>
-                <button type="submit" class ="btn btn-dark">LOGIN</button>
-                <button type="submit" class ="btn btn-dark">REGISTER</button>
+                <button type="submit" className ="btn btn-dark">LOGIN</button>
+                <button type="submit" className ="btn btn-dark">REGISTER</button>
             </form>
         </div>
     </main>
