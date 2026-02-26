66a22115c117e98c8a0cd7953d266a761f22105b
Emery Maxwell <emerymaxwell8@gmail.com>
2026-02-26 12:19:56 -0700

    set username and password

--- src/login/unauthenticated.jsx
+++ src/login/unauthenticated.jsx
@@ -4,27 +4,31 @@ import React from 'react';
 export function Unauthenticated(props) {
 
     const [userName, setUserName] = React.useState(props.userName);
-      const [password, setPassword] = React.useState('');
-      const [displayError, setDisplayError] = React.useState(null);
+    const [password, setPassword] = React.useState('');
 
     async function loginUser() {
         localStorage.setItem('userName', userName);
         props.onLogin(userName);
     }
 
-    
+    async function registerUser() {
+        localStorage.setItem('userName', userName);
+        props.onLogin(userName);
+    }
+
+
     return (
         <form method="get" action="post">
                 <div className="mb-3 input-group">
                     <span className="input-group-text">EMAIL</span>
-                    <input className="form-control" type="text" placeholder="name@email.com" />
+                    <input className="form-control" type="text" onChange={(e) => setUserName(e.target.value)} placeholder="name@email.com" />
                 </div>
                 <div className="mb-3 input-group">
                     <span className="input-group-text">PASSWORD</span>
-                    <input className="form-control" type="password" placeholder="password" />
+                    <input className="form-control" type="password" onChange={(e) => setPassword(e.target.value)} placeholder="password" />
                 </div>
                 <button  type="submit" className ="me-2 btn btn-dark" onClick={() => loginUser()} disabled={!userName || !password}>LOGIN</button>
-                <button  type="submit" className ="btn btn-dark">REGISTER</button>
+                <button  type="submit" className ="btn btn-dark" onClick={() => registerUser()} disabled={!userName || !password}>REGISTER</button>
             </form>
     )
 }
