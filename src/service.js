4a917077f413de0b3f848063256bd3acf618f060
Emery Maxwell <emerymaxwell8@gmail.com>
2026-02-26 14:26:47 -0700

    create getName and getPlan in service.js

--- src/service.js
+++ src/service.js
@@ -8,3 +8,20 @@ export function getImage() {
     return images[randomIndex];
 }
 
+export function getName() {
+    const names = ["Lily", "Danny", "Spencer", "Mia", "Noah"];
+    const randomIndex = Math.floor(Math.random() * names.length);
+    return names[randomIndex];
+}
+
+export function getPlan() {
+    const plans = [
+        "I'm making spaghetti",
+        "McDonalds!",
+        "Trying Pancakes",
+        "I'm making a salad",
+        "I'm making pizza"
+    ];
+    const randomIndex = Math.floor(Math.random() * plans.length);
+    return plans[randomIndex];
+}
