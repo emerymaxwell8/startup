0d2469c44310929cad1d7bec5b343e24af85e023
Emery Maxwell <emerymaxwell8@gmail.com>
2026-02-26 12:31:19 -0700

    create buttons

--- src/login/authenticated.jsx
+++ src/login/authenticated.jsx
@@ -2,5 +2,13 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export function Authenticated(props) {
+    const navigate = useNavigate();
 
+    return (
+        <div>
+              <div className='playerName'>{props.userName}</div>
+              <button  type="submit" className ="me-2 btn btn-dark" onClick={() => navigate('/post')}>POST</button>
+                <button  type="submit" className ="btn btn-dark" onClick={() => registerUser()} disabled={!userName || !password}>REGISTER</button>
+        </div>
+    )
 }
