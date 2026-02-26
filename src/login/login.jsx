f23d6a428f01eb65807354256a61063976e8b09f
Emery Maxwell <emerymaxwell8@gmail.com>
2026-02-26 12:37:15 -0700

    add authenticated to login

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -10,6 +10,10 @@ export function Login({ userName, authState, onAuthChange }) {
         <div><img className="picture" alt = "pizza" src="pizza-holder.jpg" width = {300}px /></div>
         <div>
             {authState !== AuthState.Unknown && <h1 id='title-main' className="title">Welcome!</h1>}
+            {authState === AuthState.Authenticated && (
+                      <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
+                    )}
+                    
             {authState === AuthState.Unauthenticated && (
                       <Unauthenticated
                         userName={userName}
