96f894975104e3890a135b9793110e0d49f5fd3f
Emery Maxwell <emerymaxwell8@gmail.com>
2026-02-26 12:33:44 -0700

    create logout

--- src/login/authenticated.jsx
+++ src/login/authenticated.jsx
@@ -4,11 +4,16 @@ import { useNavigate } from 'react-router-dom';
 export function Authenticated(props) {
     const navigate = useNavigate();
 
+    function logout() {
+        localStorage.removeItem('userName');
+        props.onLogout();
+    }
+
     return (
         <div>
               <div className='playerName'>{props.userName}</div>
               <button  type="submit" className ="me-2 btn btn-dark" onClick={() => navigate('/post')}>POST</button>
-                <button  type="submit" className ="btn btn-dark" onClick={() => registerUser()} disabled={!userName || !password}>REGISTER</button>
+                <button  type="submit" className ="btn btn-dark" onClick={() => logout()}>LOGOUT</button>
         </div>
     )
 }
