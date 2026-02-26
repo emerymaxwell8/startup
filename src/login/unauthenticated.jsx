a9235fc784d25399608f49b58bae738cfc6a69c8
Emery Maxwell <emerymaxwell8@gmail.com>
2026-02-26 13:02:57 -0700

    get rid of extra stuff in login

--- src/login/unauthenticated.jsx
+++ src/login/unauthenticated.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import { MessageDialog } from './messageDialog';
-
 
 export function Unauthenticated(props) {
 
@@ -9,28 +7,17 @@ export function Unauthenticated(props) {
     const [password, setPassword] = React.useState('');
 
     async function loginUser() {
-        if (userName === localStorage.getItem('userName') && password === localStorage.getItem('password')) {
-            props.onLogin(userName);
-        }
-        else {
-            setDisplayError('Invalid username or password');
-        }
+        localStorage.setItem('userName', userName);
+        props.onLogin(userName);
     }
 
     async function registerUser() {
-
-        if (localStorage.getItem('userName')) {
-            setDisplayError('User already exists');
-            return;
-        }
         localStorage.setItem('userName', userName);
-        localStorage.setItem('password', password);
         props.onLogin(userName);
     }
 
 
     return (
-        <>
         <form method="get" action="post">
                 <div className="mb-3 input-group">
                     <span className="input-group-text">EMAIL</span>
@@ -43,8 +30,5 @@ export function Unauthenticated(props) {
                 <button  type="submit" className ="me-2 btn btn-dark" onClick={() => loginUser()} disabled={!userName || !password}>LOGIN</button>
                 <button  type="submit" className ="btn btn-dark" onClick={() => registerUser()} disabled={!userName || !password}>REGISTER</button>
             </form>
-
-            <MessageDialog message={displayError} onHide={() => setDisplayError(null)} />
-        </>
     )
 }
