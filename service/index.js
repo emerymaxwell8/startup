7c6bba1aa3057f79fcda6dccb6a77e8b51cc5821
Emery Maxwell <emerymaxwell8@gmail.com>
2026-03-10 21:52:59 -0600

    add error handler and default page endpoints

--- service/index.js
+++ service/index.js
@@ -83,6 +83,16 @@ apiRouter.post('/favorites', verifyAuth, (req, res) => {
   res.send(favorites);
 });
 
+// Default error handler
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
+});
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
