e3217e520259769d9ddda0d7fb2270120726823e
Emery Maxwell <emerymaxwell8@gmail.com>
2026-03-11 11:52:52 -0600

    get rid of getImage in service.js

--- src/service.js
+++ src/service.js
@@ -1,13 +1,3 @@
-const images = [
-    "burger-holder.jpg", "pasta-holder.jpg", "pizza-holder.jpg", 
-    "bread-meat.jpg", "salmonbowl.jpg", "shrimp.jpg", "waffles.jpg"
-]
-
-export function getImage() {
-    const randomIndex = Math.floor(Math.random() * images.length);
-    return images[randomIndex];
-}
-
 export function getName() {
     const names = ["Lily", "Danny", "Spencer", "Mia", "Noah"];
     const randomIndex = Math.floor(Math.random() * names.length);
