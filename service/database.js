c0d6c9e9d2070f90484c955c82bd70f36165c2fe
Emery Maxwell <emerymaxwell8@gmail.com>
2026-03-24 19:35:40 -0600

    add function that tests connection with database

--- service/database.js
+++ service/database.js
@@ -7,3 +7,13 @@ const db = client.db('tasty-meals');
 const userCollection = db.collection('user');
 const postCollection = db.collection('post');
 const favoriteCollection = db.collection('favorite');
+
+(async function testConnection() {
+  try {
+    await db.command({ ping: 1 });
+    console.log(`Connect to database`);
+  } catch (ex) {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  }
+})();
