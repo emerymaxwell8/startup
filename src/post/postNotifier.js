e0577cb6e755a5bb94e8b7d6ff5add3d989fdb2b
Emery Maxwell <emerymaxwell8@gmail.com>
2026-04-07 19:51:44 -0600

    create messages for when the page is opened and closed

--- src/post/postNotifier.js
+++ src/post/postNotifier.js
@@ -14,5 +14,11 @@ class PostNotifier {
         const protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
         this.socket = new WebSocket(`${protocol}://${window.location.host}/post-notifier`);
 
-    }  
+        this.socket.onopen = (event) => {
+            this.recieveMessage(new PostMessage('Tastymeals', 'connection', {msg: 'Connected'}));
+        };
+        this.socket.onclose = (event) => {
+            this.recieveMessage(new PostMessage('Tastymeals', 'connection', {msg: 'Disconnected'}));
+        };
+    }
 }
